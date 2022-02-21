4b40faf2eb448702886655a69f15ac28538e6ccd
kavyasom <36811663+kavyasomepalli@users.noreply.github.com>
2022-02-21 12:32:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,6 @@ def greet(username):
     return f"Welcome {username}"
 
 
-app.run(host= '0.0.0.0', port=5000)
+# app.run(host= '0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
